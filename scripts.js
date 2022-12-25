e32ca94fb6214a5e48f9d63235d403e3a3cd6903
John <jmhearn@hmc.edu>
2022-12-25 09:29:13 -1000

    addNumber and deleteDigit working

--- scripts.js
+++ scripts.js
@@ -1,6 +1,11 @@
 let displayNum = 0;
-function addNumber() {
-  //   displayNum *= 10;
-  //   displayNum += this.id.parseFloat();
-  console.log(this);
+function addNumber(e) {
+  displayNum *= 10;
+  displayNum += parseFloat(e);
+  console.log(displayNum);
+}
+
+function deleteDigit() {
+  displayNum = Math.floor(displayNum / 10);
+  console.log(displayNum);
 }
