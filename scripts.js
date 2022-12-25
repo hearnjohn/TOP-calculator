6f82fbb25e72566a2cf62fe6964ed58ab280cad0
John <jmhearn@hmc.edu>
2022-12-25 10:13:36 -1000

    added operators to expressions

--- scripts.js
+++ scripts.js
@@ -1,11 +1,27 @@
-let displayNum = 0;
-function addNumber(e) {
-  displayNum *= 10;
-  displayNum += parseFloat(e);
-  console.log(displayNum);
+let displayString = "";
+function addChar(e) {
+  let strLen = displayString.length;
+  // Reject operators if the last char wasn't a number
+  if (e === "+" || e === "-" || e === "×" || e === "÷") {
+    if (
+      strLen === 0 ||
+      displayString[strLen - 1] === "+" ||
+      displayString[strLen - 1] === "-" ||
+      displayString[strLen - 1] === "×" ||
+      displayString[strLen - 1] === "÷"
+    ) {
+      return;
+    }
+  }
+  displayString += e;
+  console.log(displayString);
 }
 
-function deleteDigit() {
-  displayNum = Math.floor(displayNum / 10);
-  console.log(displayNum);
+function deleteChar() {
+  displayString = displayString.slice(0, displayString.length - 1);
+  console.log(displayString);
 }
+
+function processInput() {}
+
+function operate(num1, num2, operator) {}
