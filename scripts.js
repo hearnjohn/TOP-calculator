2f4fddc39fddb378dc84b43e07f09d84fde80cd2
John <jmhearn@hmc.edu>
2022-12-25 11:53:28 -1000

    adding operator functionality

--- scripts.js
+++ scripts.js
@@ -2,13 +2,14 @@ let displayString = "";
 function addChar(e) {
   let strLen = displayString.length;
   // Reject operators if the last char wasn't a number
-  if (e === "+" || e === "-" || e === "×" || e === "÷") {
+  if (e === "+" || e === "-" || e === "×" || e === "÷" || e === "^") {
     if (
       strLen === 0 ||
       displayString[strLen - 1] === "+" ||
       displayString[strLen - 1] === "-" ||
       displayString[strLen - 1] === "×" ||
-      displayString[strLen - 1] === "÷"
+      displayString[strLen - 1] === "÷" ||
+      displayString[strLen - 1] === "^"
     ) {
       return;
     }
@@ -22,6 +23,73 @@ function deleteChar() {
   console.log(displayString);
 }
 
-function processInput() {}
+function processInput() {
+  // Break displayString into smaller expressions, pass into operate function
+  while (
+    hasOperator("+") ||
+    hasOperator("-") ||
+    hasOperator("×") ||
+    hasOperator("÷") ||
+    hasOperator("^")
+  ) {
+    // Follow PEMDAS
+    while (hasOperator("^")) {
+      operate("^");
+    }
+    while (hasOperator("×")) {
+      operate("×");
+    }
+    while (hasOperator("÷")) {
+      operate("÷");
+    }
+    while (hasOperator("+")) {
+      operate("+");
+    }
+    while (hasOperator("-")) {
+      operate("-");
+    }
+  }
+}
 
-function operate(num1, num2, operator) {}
+function operate(operator) {
+  // Divide displayString into two, cutting out the numbers and operator of interest
+  let strLen = displayString.length;
+  let opInd = 0;
+
+  while (opInd < strLen) {
+    if (displayString[opInd] === operator) break;
+    ++opInd;
+  }
+  if (opInd == strLen) return; // Case where there is no operator of interest present
+
+  let leftP = opInd - 1,
+    rightP = opInd + 1;
+
+  while (
+    leftP >= 0 &&
+    (displayString[leftP] !== "+" ||
+      displayString[leftP] !== "-" ||
+      displayString[leftP] !== "×" ||
+      displayString[leftP] !== "÷")
+  ) {
+    --leftP;
+  }
+
+  while (
+    rightP < strLen &&
+    (displayString[rightP] !== "+" ||
+      displayString[rightP] !== "-" ||
+      displayString[rightP] !== "×" ||
+      displayString[rightP] !== "÷")
+  ) {
+    ++rightP;
+  }
+}
+
+function hasOperator(operator) {
+  let strLen = displayString.length;
+  for (let i = 0; i < strLen; ++i) {
+    if (displayString[i] === operator) return true;
+  }
+  return false;
+}
